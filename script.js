c8b43ce56e20e339ee840e7b48d8c01e2b658acb
Richard <richardjalba@gmail.com>
2020-12-07 18:46:20 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const screens = document.querySelectorAll('.screen');
+const choose_ghost_btn = document.querySelectorAll('.choose-ghost-btn');
+const start_btn = document.getElementById('start-btn');
+const game_container = document.getElementById('game-container');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const message = document.getElementById('message');
+
+let seconds = 0;
+let score = 0;
+let selected_ghost = {};
+
+// Start the game after pressing start
+start_btn.addEventListener('click', () => {
+  screens[0].classList.add('up');
+});
+
+// Choose your character and start playing
+choose_ghost_btn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img');
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    selected_ghost = { src, alt };
+    screens[1].classList.add('up');
+    setTimeout(createGhost, 1000);
+    startGame();
+  });
+});
+
+function startGame() {
+  setInterval(increaseTime, 1000);
+}
+
+function increaseTime() {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeEl.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+}
+
+//
+function createGhost() {
+  const ghost = document.createElement('div');
+  ghost.classList.add('ghost');
+  const { x, y } = getRandomLocation();
+  ghost.style.top = `${y}px`;
+  ghost.style.left = `${x}px`;
+  ghost.innerHTML = `<img src="${selected_ghost.src}" alt="${
+    selected_ghost.alt
+  }" style="transform: rotate(${Math.random() * 360}deg)" />`;
+
+  ghost.addEventListener('click', catchGhost);
+
+  game_container.appendChild(ghost);
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+}
+
+function catchGhost() {
+  increaseScore();
+  this.classList.add('caught');
+  setTimeout(() => this.remove(), 2000);
+  addGhosts();
+}
+
+function addGhosts() {
+  setTimeout(createGhost, 1200);
+  setTimeout(createGhost, 1700);
+}
+
+function increaseScore() {
+  score++;
+  if (score > 29) {
+    message.classList.add('visible');
+  }
+  scoreEl.innerHTML = `Score: ${score}`;
+}
